6d4527220d232de242d691e8ff88c3986a450aaf
Duc Vu <de.yd315@gmail.com>
2019-04-26 00:47:46 -0500

    first commit

---
+++ data.js
@@ -0,0 +1,48 @@
+const sentences = [
+    {
+        vi: 'Người thông minh không muốn tranh luận với những người không có tố chất. Nếu họ không xứng làm đối thủ của bạn, thì chỉ cần mỉm cười và rời xa họ, đừng để họ tấn công và gây cho bạn những vết thương không đáng có',
+        en: 'Smart people don’t debate with those who are ignorant. If someone is not worthy to be your opponent, all you need to do is smile and leave. Don’t let him attack and leave you the unnecessary wounds.'
+    },
+]
+
+const paragraphs = [
+    [
+        {
+            vi: 'Nuôi con trai trong sự thiếu thốn, nuôi con gái trong sự sung túc.',
+            en: `Raise a son in destituteness, but a daughter in wealth.`
+        },
+        {
+            vi: `Ba điều nên tránh khi nuôi con trai`,
+            en: `3 things to avoid when raising a son.`
+        },
+        {
+            vi: `Đừng guôi con trai trong sự sung túc`,
+            en: `Don't raise a son in wealth`
+        },
+        {
+            vi: `Ý nghĩa quan trọng của “nghèo” nuôi con trai là thông qua cảm biến sự nghèo khó và gian khổ của bản thân mà từ đó rèn luyện và tu dưỡng phẩm chất, ý chí cho con`,
+            en: `The importance of raising a son in destituteness is training and enriching your son’s will and qualities by letting him experience hardships and neediness.`
+        },
+        {
+            vi: `Điều này có nghĩa, khi giáo dục con trai nên nghiêm khắc hơn, đặc biệt đối với vật chất.`,
+            en: `This means that we should be stricter about our sons than our daughters, especially when it comes to material world.`
+        },
+        {
+            vi: `Hãy để con chịu khổ chút ít, để con trải nghiệm thất bại, rèn luyện tính tự lập và có trách nhiệm`,
+            en: `Let him experience hardships as well as failures so that he will grow up to be independent and responsible.`
+        },
+        {
+            vi: `Ba mẹ nên tách con trai khỏi mình sớm hơn con gái, đặc biệt không nên để con trai quá quyến luyến mẹ.`,
+            en: `A son should be separated from his parents earlier than a daughter, and most importantly, don’t let a son become too clingy to his mother.`
+        },
+        {
+            vi: `Trẻ trai tách dần khỏi ba mẹ sẽ sớm học cách quyết đoán khi làm việc, như vậy khi lớn lên sẽ trở nên độc lập và dễ dàng thích nghi với xã hội này.`,
+            en: `Baby boys who are gradually separated from their parents will soon learn to become decisive, therefore they will grow up to be independent and adapt easily to this society.`
+        },
+        {
+            en: `Quite a few parents think that there's nothing wrong with their sons being messy.`,
+            vi: `Một số cha mẹ nghĩ rằng con trai lôi thôi không có vấn đề gì. `
+        }
+    ]
+]
+
